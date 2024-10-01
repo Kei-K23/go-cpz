4be986df1d0bf9bf3455e9b025f8fbacbd37cfde
Kei-K23 <keiksl2301@gmail.com>
2024-10-01 18:18:34 +0630

    Add concurrency for handle large file and copying and moving directory

--- internal/lib/copy.go
+++ internal/lib/copy.go
@@ -5,17 +5,26 @@ package lib
 
 import (
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"sync"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
-func Copy(src, desc string) error {
+// Limit the number of concurrent copy operations
+var semaphore = make(chan struct{}, 5) // Adjust the size as needed to control concurrency
+
+func Copy(src, dest string) error {
 	// Get the source information
 	info, err := os.Stat(src)
 	if err != nil {
 		return err
 	}
+
+	var wg sync.WaitGroup
 
 	if info.IsDir() {
 		// Handle directory copy
@@ -25,23 +34,71 @@ func Copy(src, desc string) error {
 			}
 
 			// Get the destination path
-			destPath := filepath.Join(desc, path[len(src):])
+			destPath := filepath.Join(dest, path[len(src):])
 
 			// If encounter dir while current source file walk through
 			if info.IsDir() {
 				// Create all those file and folder in the newly created destination folder path
 				return os.MkdirAll(destPath, info.Mode())
-			} else {
-				return copyFile(path, destPath)
 			}
+			wg.Add(1) // Increment the wait group to track the goroutines
+			return copyFile(path, destPath, &wg)
 		})
+	} else {
+		// Copy normal file
+		wg.Add(1) // Increment the wait group to track the goroutines
+		err = copyFile(src, dest, &wg)
 	}
 
-	// Copy normal file
-	return copyFile(src, desc)
+	wg.Wait() // Wait all goroutines to finish
+	return err
 }
 
-func copyFile(src, desc string) error {
-	fmt.Printf("Copy from %s to %s\n", src, desc)
+func copyFile(src, dest string, wg *sync.WaitGroup) error {
+	semaphore <- struct{}{}
+
+	// Start the goroutine
+	go func() {
+		// Decrement the wait group counter when done this goroutines
+		defer wg.Done()
+		defer func() { <-semaphore }() // Release the semaphore slot
+
+		// Open the source file
+		srcFile, err := os.Open(src)
+		if err != nil {
+			fmt.Printf("Error opening source file: %v\n", err)
+			return
+		}
+		defer srcFile.Close()
+
+		// Create destination file
+		destFile, err := os.Create(dest)
+		if err != nil {
+			fmt.Printf("Error creating destination file: %v\n", err)
+			return
+		}
+		defer destFile.Close()
+
+		// Get the metadata of source file
+		srcInfo, err := srcFile.Stat()
+		if err != nil {
+			fmt.Printf("Error source file information: %v\n", err)
+			return
+		}
+
+		// Create progress indicator
+		pbBar := pb.Full.Start64(srcInfo.Size())
+		pbBarRdr := pbBar.NewProxyReader(srcFile)
+
+		// Start copying the file
+		_, err = io.Copy(destFile, pbBarRdr)
+		if err != nil {
+			fmt.Printf("Error copying file: %v\n", err)
+			return
+		}
+
+		pbBar.Finish()
+		fmt.Printf("Copied %s to %s\n", src, dest)
+	}()
 	return nil
 }
